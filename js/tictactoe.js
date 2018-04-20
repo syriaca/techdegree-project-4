9ec928d4d22a45ff036c079a518ef1dd886d7150
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-04-20 08:34:28 +0200

    Start button
    Start screen disappear and board appear

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,5 +1,6 @@
-const boardScreen = document.getElementById("board");
-const startScreen = document.getElementById("start");
+const boardScreen = document.getElementById('board');
+const startScreen = document.getElementById('start');
+const startGameButton = document.querySelector('#start .button')
 
 function elementDisplay(id, type){
     const displayType = id.style.display = type;
@@ -7,9 +8,15 @@ function elementDisplay(id, type){
 };
 
 (function(){
-    document.addEventListener("DOMContentLoaded", (e) => {
-        elementDisplay(startScreen, "block");
-        elementDisplay(boardScreen, "none");
-    });    
+    document.addEventListener('DOMContentLoaded', (e) => {
+        elementDisplay(startScreen, 'block');
+        elementDisplay(boardScreen, 'none');
+    });
+
+    startGameButton.addEventListener('click', (e) => {
+        elementDisplay(startScreen, 'none');
+        elementDisplay(boardScreen, 'block');
+    });
+
 }());
 
